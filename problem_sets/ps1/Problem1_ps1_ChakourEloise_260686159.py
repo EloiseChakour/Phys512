5bda339486098b48fcee611b010135bf270c7159
EloiseChakour <eloise.chakour@gmail.com>
2021-09-10 18:58:25 -0400

    Up to second der check

--- problem_sets/ps1/Problem1_ps1_ChakourEloise_260686159.py
+++ problem_sets/ps1/Problem1_ps1_ChakourEloise_260686159.py
@@ -5,3 +5,6 @@ Created on Fri Sep 10 15:35:51 2021
 @author: elois
 """
 
+import numpy as np
+
+
---
+++ problem_sets/ps1/Problem2_ps1_ChakourEloise_260686159.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 10 17:21:24 2021
+
+@author: elois
+"""
+
+import numpy as np
+
+
+
+epsilon = 10**(-15)
+sqrtEps = np.sqrt(epsilon)
+x = 1.0
+
+#Take a first estimate for the first derivative using an arbitrary dx value
+def estimateFirstDer(fun, x, stepEst):
+    newX = x + stepEst
+    stepEst = newX- x
+    firstDer = (fun(x+stepEst) - fun(x-stepEst))/(2*stepEst)
+    return firstDer
+
+#Testting
+estimate1 = estimateFirstDer(np.exp, x, sqrtEps)
+trueVal = np.exp(x)
+
+print("Testing First Derivative")
+print(estimate1)
+print(trueVal)
+print(estimate1-trueVal)
+
+
+
+
+
+def estimateSecondDer(fun, x, stepEst):
+    newX = x + stepEst
+    stepEst = newX-x
+    #secondDer = (estimateFirstDer(fun, x+stepEst, stepEst) - estimateFirstDer(fun, x-stepEst, stepEst))/(2*stepEst)
+    secondDer = (fun(x + 2*stepEst) + fun(x - 2*stepEst) - 2*fun(x) )/(4*stepEst**2)
+    return secondDer
+
+estimate2 = estimateSecondDer(np.exp, x, sqrtEps)
+
+print("Testing Second Derivative")
+print(estimate2)
+print(trueVal)
+print(estimate2-trueVal)
+
+
+def newStepEst(fun, x, epsilon):
+    newStep = np.sqrt(epsilon*(fun(x)/ estimateSecondDer(fun, x, np.sqrt(epsilon))))
+    return newStep
+
+newStep = newStepEst(np.exp, x, epsilon)
+
+print("This is the new dx size")
+print(newStep)
+
+newSecond = estimateSecondDer(np.exp, 1, newStep)
+print("Testing Second Derivative with new Step Size")
+print(newSecond)
+print(trueVal)
+print(newSecond-trueVal)
+
+
+
