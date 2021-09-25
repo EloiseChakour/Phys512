b7e3c599b07c28399bd0902926e8adbac92f279d
EloiseChakour <eloise.chakour@gmail.com>
2021-09-25 00:57:06 -0400

    Updated Problem 3

--- problem_sets/ps2/Problem3_ps2_ChakourEloise_260686159.py
+++ problem_sets/ps2/Problem3_ps2_ChakourEloise_260686159.py
@@ -47,25 +47,55 @@ def mylog(x, pts, ord_Guess = 10):
     value = np.polynomial.chebyshev.chebval(x, coeffs)
     return value, order
 
-
+#Similar to evaluateCheb1 but for log
 def evaluateCheb2(pts, ord_Guess):
+    x_log = np.linspace(0.001, 10000, pts)
+    y = np.log(x_log)
+    x_cheb = x_log
+    cheb = np.polynomial.chebyshev.chebfit(x_cheb,y, ord_Guess)
     
+    x_test = np.linspace(0.001, 10000, pts + 20)
+    cheb_evaluated = np.polynomial.chebyshev.chebval(x_test, cheb)
+    ref_vals = np.log(x_test)
+    error = np.abs(cheb_evaluated - ref_vals)
+    accuracy = np.average(error)
     
-    
-    
-    
-    return 
+    if accuracy >= 1e-6:
+       ord_Guess  = ord_Guess + 1
+       return evaluateCheb1(pts, ord_Guess)
+    elif (accuracy <= 1e-6) and (accuracy >= 1e-7):
+       return cheb, ord_Guess, accuracy
+    elif accuracy < 1e-7:
+        ord_Guess = ord_Guess - 1
+        return evaluateCheb1(pts, ord_Guess)
 
-def mylog2(x):
-    #stub
-    return
+#Similar to mylog but for log
+def mylog2(x, pts, ord_Guess = 10):
+    #Check for a positive number
+    if x<=0.0: 
+        sys.exit("Please enter a positive number")
+    #Compute the coefficients for the Chebyshev Polynomial
+    coeffs, order, accuracy = evaluateCheb1(pts, ord_Guess)
+    value = np.polynomial.chebyshev.chebval(x, coeffs)
+    
+    return value, order
+
+#Testing Value
+x_small = 0.8 #Must be between 0.5 and 1
+x_large = 10 #Positive Number
+pts = 100 #Nb poinst to use for the fit
 
 
+#Compute the Values
+val_log2, order = mylog(x_small, pts)
+val_log, order_log = mylog2(x_large, pts)
 
-val_log2, order = mylog(0.6, 10)
-print("The value obtained was", val_log2)
+
+#Print the Results
+print("The value (log_2 for x_small) obtained was", val_log2)
 print("The order of the polynomial that was used was", order)
-
+print("The value (log for x_large) obtained was", val_log)
+print("The order of the polynomial that was used was", order_log)
 
 
 
