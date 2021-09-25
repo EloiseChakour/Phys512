d9cc8dc0926e5557cce3557db927e4bfcd1394f7
EloiseChakour <eloise.chakour@gmail.com>
2021-09-24 20:26:37 -0400

    Current log works

--- problem_sets/ps2/Problem2_ps2_ChakourEloise_260686159.py
+++ problem_sets/ps2/Problem2_ps2_ChakourEloise_260686159.py
@@ -7,19 +7,91 @@ Created on Wed Sep 22 14:21:28 2021
 
 
 import numpy as np
+import sys
+
+def integrate_adaptive(fun, a, b, tol, extra = None):
+    print("Integrating between", a, b)
+    
+    if type(extra) != np.ndarray:
+        old_error = 0
+        counter = 0
+        failed_iterations = 0
+    else:
+        old_error = extra[0]
+        counter = extra[1]
+        failed_iterations = extra[2]
+
+        
+    
+    x = np.linspace(a, b, 5)
+    y = fun(x)
+    dx = (x[1]-x[0])/(len(x)-1)
+    
+    coarse_area = 2*dx*(y[0]+4*y[2]+y[4])/3
+    fine_area = dx*(y[0]+4*y[1]+2*y[2]+4*y[3]+y[4])/3
+
+    
+    error = np.abs(coarse_area-fine_area)
+    total_area = fine_area
+    
+    counter += 1
 
 
 
 
 
 
+    #Return appropriate 
+    if error <= tol: 
+        #Return the result of the integration if the error is smaller than the tolerance
+        return total_area, error, counter, failed_iterations
+    elif np.abs(error-old_error) <= 10**-14 :
+        #Check to see if the error was appreciably changed by the last iteration and if the counter exists.
+        if failed_iterations >= 3:
+            #If there have been at least 3 iterations that have not changed the error, quit and send an error message
+            sys.exit("There have been %s failed iterations. The current area is %d and the current error is %f." %(counter, total_area, error))
+        else:
+            #If there have not been 3 failed iterations, add 1 to the failed iteration counter
+            failed_iterations += 1
+            #Do another integration loop
+            info = np.zeros(3)
+            info[0] = error
+            info[1] = counter
+            info[2] = failed_iterations
+            new_x = (a+b)/2.0
+            area1, error1, counter1, failed_iterations1 = integrate_adaptive(fun, a, new_x, tol/2, info)
+            area2, error2, counter2, failed_iterations2 = integrate_adaptive(fun, new_x, b, tol/2, info)
+            total_area += area1 + area2
+            counter = counter1
+            return total_area, error, counter, failed_iterations
+    else:
+        #If there have not been more than 3 failed iterations and the error is not within tolerance, record and return the relevant information for a new iteration
+        info = np.zeros(3)
+        info[0] = error
+        info[1] = counter
+        info[2] = failed_iterations
+        #Do another integration loop
+        new_x = (a+b)/2.0
+        area1, error1, counter1, failed_iterations1 = integrate_adaptive(fun, a, new_x, tol/2, info)
+        area2, error2, counter2, failed_iterations2 = integrate_adaptive(fun, new_x, b, tol/2, info)
+        total_area += area1 + area2
+        counter = counter1
+        return total_area, error, counter, failed_iterations
 
 
 
+def lorentz(x):
+    lor = 1.0/(1.0 + x**2)
+    return lor
 
+def linear(x):
+    return x
 
-
-
+area, error, counter, failed_iterations = integrate_adaptive(lorentz, 0, 1, 1e-7)
+print("The area is", area)
+print("The error is", error)
+print("The number of iterations was", counter)
+print("The number of failed iterations was", failed_iterations)
 
 
 
--- problem_sets/ps2/Problem3_ps2_ChakourEloise_260686159.py
+++ problem_sets/ps2/Problem3_ps2_ChakourEloise_260686159.py
@@ -10,14 +10,46 @@ import numpy as np
 
 
 
+def mylog(pts, ord_Guess = 10):
+    x_log = np.linspace(0.5, 1.0, pts)
+    y = np.log2(x_log)
+    x_cheb = x_log
+    cheb = np.polynomial.chebyshev.chebfit(x_cheb,y, ord_Guess)
+    
+    x_test = np.linspace(0.5, 1.0, pts + 20)
+    cheb_evaluated = np.polynomial.chebyshev.chebval(x_test, cheb)
+    ref_vals = np.log2(x_test)
+    error = np.abs(cheb_evaluated - ref_vals)
+    accuracy = np.average(error)
+    
+    if accuracy >= 1e-6:
+       ord_Guess  = ord_Guess + 1
+       return mylog(pts, ord_Guess)
+    elif (accuracy <= 1e-6) and (accuracy >= 1e-7):
+       return cheb, ord_Guess, accuracy
+    elif accuracy < 1e-7:
+        ord_Guess = ord_Guess - 1
+        return mylog(pts, ord_Guess)
+    else:
+        print("Hello")
+        return 2
 
 
 
 
 
+def mylog2(x):
+    #stub
+    return
 
 
 
+
+
+cheb, order, accuracy =mylog(20)
+print("The coefficients are", cheb)
+print("The order is", order)
+print("The accuracy is", accuracy)
 
 
 
