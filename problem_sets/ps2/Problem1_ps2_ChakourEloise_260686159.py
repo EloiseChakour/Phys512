b2b9826793d66be53d8a7b34c5da58a2877da1cf
EloiseChakour <eloise.chakour@gmail.com>
2021-09-24 21:29:59 -0400

    New Gauss

--- problem_sets/ps2/Problem1_ps2_ChakourEloise_260686159.py
+++ problem_sets/ps2/Problem1_ps2_ChakourEloise_260686159.py
@@ -6,16 +6,46 @@ Created on Wed Sep 22 14:21:32 2021
 """
 
 import numpy as np
+from scipy import integrate
+import matplotlib.pyplot as plt
+
+def computeConstant(R, s):
+    e = 8.8541878128* 10**(-12)
+    constant = (2*np.pi*R*R*s)/(4.0*np.pi*e)
+    return constant
+
+def toIntegrate(x, z, R):
+    num = z-R*x
+    denom = (R*R + z*z - 2.0*R*z*x)
+    func = num/denom
+    return func
+
+
+def evalQuad(fun, z, R, s, a = -1, b = 1):
+    integral = integrate.quad(fun, a, b, args=(z, R))
+    return integral
 
 
 
+def getQuadVals(z_min, z_max, fun, R, s):
+    pts = (z_max-z_min)*10
+    z_arr = np.linspace(z_min, z_max, pts)
+    int_Vals = []
+    for i in range(pts):
+        integral_Val, err = evalQuad(fun, z_arr[i], R, s)
+        int_Vals.append(integral_Val)
+    return z_arr, int_Vals
 
+R = 50.0
+s = 2.0
 
+z, E = getQuadVals(0, 100, toIntegrate, R, s)
 
-
-
-
-
+plt.figure(num=1, figsize=(4,4))
+plt.title('Electrical Field')
+plt.axvline(x=50.0, color='r')
+plt.plot(z, E, label='Quad')
+plt.legend()
 
 
 
--- problem_sets/ps2/Problem3_ps2_ChakourEloise_260686159.py
+++ problem_sets/ps2/Problem3_ps2_ChakourEloise_260686159.py
@@ -6,11 +6,13 @@ Created on Wed Sep 22 14:22:17 2021
 """
 
 import numpy as np
+import sys
 
 
-
-
-def mylog(pts, ord_Guess = 10):
+#This function fits a chebyshev polynomial for a log_2 between 0.5 and 1.
+#Inputs: number of points to use for the Chebyshev fit (pts) and the initial guess for the order of the Chebyshev polynomial to fit (ord_Guess). 
+#Outputs: The Chebyshev coefficients, the order of the polynomial and the average error found in testing with this fit (accuracy)
+def evaluateCheb1(pts, ord_Guess):
     x_log = np.linspace(0.5, 1.0, pts)
     y = np.log2(x_log)
     x_cheb = x_log
@@ -24,19 +26,35 @@ def mylog(pts, ord_Guess = 10):
     
     if accuracy >= 1e-6:
        ord_Guess  = ord_Guess + 1
-       return mylog(pts, ord_Guess)
+       return evaluateCheb1(pts, ord_Guess)
     elif (accuracy <= 1e-6) and (accuracy >= 1e-7):
        return cheb, ord_Guess, accuracy
     elif accuracy < 1e-7:
         ord_Guess = ord_Guess - 1
-        return mylog(pts, ord_Guess)
-    else:
-        print("Hello")
-        return 2
+        return evaluateCheb1(pts, ord_Guess)
+
+#This function evaluates a Chebyshev polynomial at a certain x to extimate a log_2
+#Inputs: A value of x for which to evaluate the polynomial (x), the number of points to use for the fit (pts) and the initial guess for the order of the polynomial (ord_Guess)
+#Outputs: The estimated valye of log_2 at the given x and the order of the polynomial used
+def mylog(x, pts, ord_Guess = 10):
+    #Check to see that the value of x is valid.
+    if x > 1:
+        sys.exit("This is not a valid value of x. Please enter a value between 0.5 and 1.0.")
+    if x< 0.5:
+        sys.exit("This is not a value of x. Please enter a value between 0.5 and 1.0.")
+    #Compute the coefficients for the Chebyshev Polynomial
+    coeffs, order, accuracy = evaluateCheb1(pts, ord_Guess)
+    value = np.polynomial.chebyshev.chebval(x, coeffs)
+    return value, order
 
 
-
-
+def evaluateCheb2(pts, ord_Guess):
+    
+    
+    
+    
+    
+    return 
 
 def mylog2(x):
     #stub
@@ -44,13 +62,9 @@ def mylog2(x):
 
 
 
-
-
-cheb, order, accuracy =mylog(20)
-print("The coefficients are", cheb)
-print("The order is", order)
-print("The accuracy is", accuracy)
-
+val_log2, order = mylog(0.6, 10)
+print("The value obtained was", val_log2)
+print("The order of the polynomial that was used was", order)
 
 
 
