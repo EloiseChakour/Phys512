1e5559a1a6542344387f82dc57a44aea7713f758
EloiseChakour <eloise.chakour@gmail.com>
2021-10-05 16:25:38 -0400

    First rk4 evaluation works

--- problem_sets/ps3/Problem1_ps3_ChakourEloise_260686159.py
+++ problem_sets/ps3/Problem1_ps3_ChakourEloise_260686159.py
@@ -6,6 +6,7 @@ Created on Mon Oct  4 19:49:20 2021
 """
 
 import numpy as np
+from matplotlib import pyplot as plt
 
 
 
@@ -17,10 +18,17 @@ def func(x, y):
 
 
 
-
+#Compute one term of the rk4 integration
 def rk4_step(fun, x, y, h):
-    #stub
-    return
+    
+    k1=fun(x,y)*h
+    k2=h*fun(x+h/2,y+k1/2)
+    k3=h*fun(x+h/2,y+k2/2)
+    k4=h*fun(x+h,y+k3)
+    dy=(k1+2*k2+2*k3+k4)/6
+    
+    return y + dy
+
 
 
 def rk4_stepd(fun, x, y, h):
@@ -29,11 +37,25 @@ def rk4_stepd(fun, x, y, h):
 
 
 
+def computeRK4(fun, x_0 = -20.0, x_f = 20.0, y_0 = -1, nb_steps = 200):
+    h = (x_f-x_0)/nb_steps
+    x = np.linspace(x_0, x_f, nb_steps)
+    y = np.zeros(nb_steps)
+    y[0] = y_0
+    for i in range(nb_steps-1):
+        y[i+1] = rk4_step(fun, x[i], y[i], h)
+    return x, y
+    
 
+#Compute the solution for rk4 with given parameters and a set step size
+x, y = computeRK4(func)
 
-
-
-
+#Plot and save the figure
+plt.figure(num=1)
+plt.plot(x, y)
+plt.axvline(0, color='k')
+plt.title("RK4 Solution for 200 Steps")
+plt.savefig("rk4_200pts_setStep.jpg")
 
 
 
